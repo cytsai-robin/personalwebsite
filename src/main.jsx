9661e2a4f4b8cf31ca71c860984e872a9f238966
cytsai-robin <tjy88810@gmail.com>
2023-04-04 20:50:46 +0800

    update website

--- src/main.jsx
+++ src/main.jsx
@@ -7,22 +7,22 @@ import About from "./pages/about";
 import './index.css'
 
 const router = createHashRouter([
-{
-path: "/",
-element: <RootLayout />,
-children: [
-{ index: true, element: <RootIndex /> },
-{
-path: "/about",
-element: <About />,
-},
-],
-},
+    {
+        path: "/",
+        element: <RootLayout />,
+        children: [
+            { index: true, element: <RootIndex /> },
+            {
+                path: "/about",
+                element: <About />,
+            },
+        ],
+    },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render
-(
-<React.StrictMode>
-<RouterProvider router={router} />
-</React.StrictMode>
-);
+    (
+        <React.StrictMode>
+            <RouterProvider router={router} />
+        </React.StrictMode>
+    );
 
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,5 +1,15 @@
 export default function About() {
-    return <h1 className="text-3xl font-bold underline text-purple-500">
-        Hello world!
-    </h1>;
+    return (
+        // <h1 className="text-3XL font-semibold text-gray-900">
+        //     About
+        // </h1>
+
+        <div className="bg-gray-100">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
+                    <h2 className="text-2xl font-bold text-gray-900">nothing here!</h2>
+                </div>
+            </div>
+        </div>
+    )
 }
