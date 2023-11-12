e9ca1e43a10a00b953e2e72bc652a7d7c519fa4a
cytsai-robin <tjy88810@gmail.com>
2023-11-13 00:58:52 +0800

    LowerToUpper

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -7,7 +7,7 @@ export default function About() {
         <div className="bg-gray-100">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-                    <h2 className="text-2xl font-bold text-gray-900">nothing here!</h2>
+                    <h2 className="text-2xl font-bold text-gray-900">Nothing Here!</h2>
                 </div>
             </div>
         </div>
